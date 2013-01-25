f01bbc999acfcf618dc7849133cfdac96091ad4a
Jeff May <jeff.may@audaxhealth.com>
2013-01-25 01:55:57 -0500

    Saving state.

--- pivoter.py
+++ pivoter.py
@@ -1,3 +1,4 @@
+import sys
 
 def main(params):
     import csv
@@ -6,22 +7,85 @@ def main(params):
     #   where label is first column
     #   produce one row with a label and an index into a hashed set of labels
     #   save the file to git
-    with open(params.file) as finput:
-        input = csv.DictReader(finput)
-        pivot(input)
+    table = read_table(params.input)
+    result = pivot(table)
+    result_table = convert_to_table(result)
+    if params.output is not None:
+        with open(params.output, "w") as output:
+            dump_table(result_table, output)
+    else:
+        dump_table(result_table, sys.stdout)
 
-def pivot(table, seed=set()):
-    result = []
-    for row in table:
-        pass
-    return result
+def read_table(fname):
+    import csv
+    with open(fname) as finput:
+        reader = csv.reader(finput)
+        table = [tuple([c.strip() for c in row]) for row in reader]
+    return table
 
-def parse_column_descriptors(row):
+
+def pivot(table, seed={}, start_index=1):
+    _, month_dates = parse_header(table[0])
+    rotated = rotate(table[1:])
+    # get the accounts from all the first cols
+    accounts = [account for account in rotated[0]]
+    num_accounts = len(accounts)
+    month_data = rotated[1:]
+#    print rotated
+    for i, data in enumerate(month_data):
+        last_index = i + start_index
+        month = format_date_time(month_dates[i])
+        account = accounts[i % num_accounts]  # months rotate over accounts
+        time_periods = data[1:]
+        key = (account, month)
+        entry = seed[key] if account in seed else Entry(last_index, month)
+        for value in time_periods:
+            entry.total += int(value)
+        seed[key] = entry
+    return seed
+
+def convert_to_table(data):
+    data = sorted(data.values())
+    rows = [tuple([entry.account_id, entry.total, entry.month_date]) for entry in data]
+    header = ("account", "total", "month_date")
+    return [header] + rows
+
+def dump_table(table, output):
+    import csv
+    writer = csv.writer(output)
+    writer.writerows(table)
+
+
+class Entry:
+    def __init__(self, account_id, month_date, total=0):
+        self.account_id = account_id
+        self.month_date = month_date
+        self.total = total
+
+    def __eq__(self, other):
+        return isinstance(other, Entry) and self.__dict__ == other.__dict__
+
+    def __hash__(self):
+        return sum((hash(item) for item in self.__dict__.items()))
+
+    def __repr__(self):
+        return "Entry(%s)" % ", ".join([str(self.account_id), str(self.total)])
+
+    def __cmp__(self, other):
+        diff_accounts = cmp(self.account_id, other.account_id)
+        if diff_accounts:
+            return diff_accounts
+        else:
+            return cmp(self.month_date, other.month_date)
+
+def rotate(matrix):
+    return zip(*matrix[:])
+
+def parse_header(row):
     label = row[0]
     str_cols = row[1:]
     date_cols = [parse_date_time(col) for col in str_cols]
-    formatted_cols = [format_date_time(col) for col in date_cols]
-    return [label] + formatted_cols
+    return label, date_cols
 
 def format_date_time((month, year)):
     return '%4d-%02d' % (year, month)
@@ -48,11 +112,11 @@ def parse_date_time(date_time):
 def parse_cli(args):
     import argparse
     parser = argparse.ArgumentParser("pivoter")
-    parser.add_argument("file", help="")
+    parser.add_argument("input", help="")
+    parser.add_argument("-o", "--output", help="")
     return parser.parse_args(args)
 
 
 if __name__ == '__main__':
-    import sys
     params = parse_cli(sys.argv[1:])
     main(params)
--- test_pivoter.py
+++ test_pivoter.py
@@ -3,13 +3,8 @@ from unittest import TestCase
 from pivoter import *
 
 def extract(fname):
-    input = read_csv(fname)
-    return list(pivot(input))
-
-def read_csv(fname):
-    import csv
-    with open(fname) as finput:
-        return list(csv.DictReader(finput))
+    input = read_table(fname)
+    return pivot(input)
 
 class TestPivoter(TestCase):
 
@@ -20,9 +15,35 @@ class TestPivoter(TestCase):
         main(parse_cli(["samples/sample_ABC.csv"]))
 
     def test_pivoter(self):
-        result = extract("samples/sample_ABC.csv")
-        expected = read_csv("samples/result_ABCD.csv")
-        self.assertItemsEqual(result, expected)
+        data = extract("samples/sample_ABC.csv")
+        result_table = convert_to_table(data)
+        expected_result = read_table("samples/result_ABCD.csv")
+        print "actual: %s" % result_table
+        print "expected: %s" % expected_result
+        for actual, expected in result_table, expected_result:
+            self.assertItemsEqual(actual, expected)
+
+    def test_rotate(self):
+        actual = rotate((("A","B","C"),(1, 2, 3)))
+        expected = [("A", 1), ("B", 2), ("C", 3)]
+        self.assertEqual(expected, actual)
+
+    def test_convert_to_table(self):
+        rows = convert_to_table({
+            3: Entry(2, "2012-05"),
+            1: Entry(1, "2012-03"),
+            0: Entry(1, "2012-01"),
+            2: Entry(2, "2012-03"),
+            4: Entry(2, "2013-03"),
+        })
+        self.assertItemsEqual([
+            Entry(1, "2012-01"),
+            Entry(1, "2012-03"),
+            Entry(2, "2012-03"),
+            Entry(2, "2012-05"),
+            Entry(2, "2013-03"),
+        ], rows)
+
 
 
 class TestColumnDescriptor(TestCase):
@@ -30,12 +51,12 @@ class TestColumnDescriptor(TestCase):
     descriptor_row = ("account","Jan-09","Feb-09","Mar-09","Apr-09","May-09","Jun-09","Jul-09","Aug-09","Sep-09","Oct-09","Nov-09","Dec-09")
 
     def test_first_col(self):
-        input = parse_column_descriptors(self.descriptor_row)
-        self.assertEqual(input[0], "account")
+        account, _ = parse_header(self.descriptor_row)
+        self.assertEqual(account, "account")
 
     def test_row_date(self):
-        input = parse_column_descriptors(self.descriptor_row)
-        self.assertEqual(input[1], "2009-01")
+        _, cols  = parse_header(self.descriptor_row)
+        self.assertEqual(cols[0], (1, 2009))
 
 class TestParseDateTime(TestCase):
 
